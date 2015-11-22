127278d2c55c7683652f5ac34238c9977c92c549
austin <austin.cawley@gmail.com>
2015-11-22 00:00:57 -0500

    Removed the Chrome Extension

--- netflixExt/NetflixAndChill/main.js
+++
@@ -1,9 +0,0 @@
-// What is injected into 
-
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    /*if( request.message === "clicked_browser_action" ) {
-      chrome.runtime.sendMessage({"message": "started_movie", "movie": "child"});
-    }*/
-  }
-);
